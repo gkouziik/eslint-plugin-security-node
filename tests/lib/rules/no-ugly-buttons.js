7f1cafe6cd4449a32d20564cbd13e78cd213806a
John Gkouz <gkouziik@gmail.com>
2019-05-14 15:15:39 +0300

    #4 The rule in lib/rules/no-ugly-buttons.js has been written,also i implement the test and all the tests passed!

--- tests/lib/rules/no-ugly-buttons.js
+++ tests/lib/rules/no-ugly-buttons.js
@@ -2,36 +2,57 @@
  * @fileoverview Buttons must have some styling
  * @author gkouziik
  */
-"use strict";
+'use strict'
 
-//------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
+const rule = require('../../../lib/rules/no-ugly-buttons')
+const RuleTester = require('eslint').RuleTester
 
-var rule = require("../../../lib/rules/no-ugly-buttons"),
+RuleTester.setDefaultConfig({
+  parserOptions: {
+    ecmaVersion: 6,
+    ecmaFeatures: {
+      jsx: true
+    }
+  }
+})
 
-    RuleTester = require("eslint").RuleTester;
+const ERROR_MSG_NOT_STYLED = 'Buttons must be styled with a btn class at least.'
 
+const ruleTester = new RuleTester()
+ruleTester.run('no-ugly-buttons', rule, {
 
-//------------------------------------------------------------------------------
-// Tests
-//------------------------------------------------------------------------------
+  valid: [{
+    code: '<button className="btn"></button>'
+  }],
 
-var ruleTester = new RuleTester();
-ruleTester.run("no-ugly-buttons", rule, {
-
-    valid: [
-
-        // give me some code that won't trigger a warning
-    ],
-
-    invalid: [
-        {
-            code: "<button><button>",
-            errors: [{
-                message: "Fill me in.",
-                type: "Me too"
-            }]
-        }
-    ]
-});
+  invalid: [{
+    code: '<button></button>',
+    errors: [{
+      message: ERROR_MSG_NOT_STYLED,
+      type: 'JSXOpeningElement'
+    }]
+  }]
+})
+// 'use strict'
+//
+// var rule = require('../../../lib/rules/no-ugly-buttons')
+// var RuleTester = require('eslint').RuleTester
+//
+// var ruleTester = new RuleTester()
+// ruleTester.run('no-ugly-buttons', rule, {
+//
+//   valid: [
+//     "var $ = require('jquery');",
+//     "var filter = require('lodash/fp/filter')"
+//   ],
+//
+//   invalid: [
+//     {
+//       code: "var _ = require('your favorite fp library');",
+//       errors: [{
+//         message: 'Prefer importing single functions over a full FP library',
+//         type: 'VariableDeclaration'
+//       }]
+//     }
+//   ]
+// })
