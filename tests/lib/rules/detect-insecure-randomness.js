c744ea8ec296a49c5c9797a040bc4488e49b5df6
John Gkouz <gkouziik@gmail.com>
2019-09-11 23:46:57 +0300

    #12 Rule Tested

--- tests/lib/rules/detect-insecure-randomness.js
+++ tests/lib/rules/detect-insecure-randomness.js
@@ -2,36 +2,25 @@
  * @fileoverview detect insecure randomness via Math.random()
  * @author Gkouziik
  */
-"use strict";
+'use strict'
 
-//------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
+var rule = require('../../../lib/rules/detect-insecure-randomness')
+var RuleTester = require('eslint').RuleTester
+const invalid = 'Math.random();'
+const ERROR_MSG = 'detect Math.random()'
+var ruleTester = new RuleTester()
+ruleTester.run('detect-insecure-randomness', rule, {
 
-var rule = require("../../../lib/rules/detect-insecure-randomness"),
+  valid: [
 
-    RuleTester = require("eslint").RuleTester;
+  ],
 
-
-//------------------------------------------------------------------------------
-// Tests
-//------------------------------------------------------------------------------
-
-var ruleTester = new RuleTester();
-ruleTester.run("detect-insecure-randomness", rule, {
-
-    valid: [
-
-        // give me some code that won't trigger a warning
-    ],
-
-    invalid: [
-        {
-            code: "",
-            errors: [{
-                message: "Fill me in.",
-                type: "Me too"
-            }]
-        }
-    ]
-});
+  invalid: [
+    {
+      code: invalid,
+      errors: [{
+        message: ERROR_MSG
+      }]
+    }
+  ]
+})
