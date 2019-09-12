c35051f418f144ef898eaf172570e3979566dee4
John Gkouz <gkouziik@gmail.com>
2019-09-12 23:48:19 +0300

    #30 Rule Tested

--- tests/lib/rules/non-literal-reg-expr.js
+++ tests/lib/rules/non-literal-reg-expr.js
@@ -2,36 +2,34 @@
  * @fileoverview Non literal regural expressions may cause possible attack
  * @author Gkouziik
  */
-"use strict";
+'use strict'
 
-//------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
+var rule = require('../../../lib/rules/non-literal-reg-expr')
+var RuleTester = require('eslint').RuleTester
+const ERROR_MSG = 'Found RegExp with non literal argument'
+const valid = 'var regE = new RegExp("www");'
+const invalid = 'var regE = new RegExp(foo);'
+const invalidSecond = 'var regE = new RegExp(foo + "www");'
+var ruleTester = new RuleTester()
+ruleTester.run('non-literal-reg-expr', rule, {
+  valid: [
+    {
+      code: valid
+    }
+  ],
 
-var rule = require("../../../lib/rules/non-literal-reg-expr"),
-
-    RuleTester = require("eslint").RuleTester;
-
-
-//------------------------------------------------------------------------------
-// Tests
-//------------------------------------------------------------------------------
-
-var ruleTester = new RuleTester();
-ruleTester.run("non-literal-reg-expr", rule, {
-
-    valid: [
-
-        // give me some code that won't trigger a warning
-    ],
-
-    invalid: [
-        {
-            code: "",
-            errors: [{
-                message: "Fill me in.",
-                type: "Me too"
-            }]
-        }
-    ]
-});
+  invalid: [
+    {
+      code: invalid,
+      errors: [{
+        message: ERROR_MSG
+      }]
+    },
+    {
+      code: invalidSecond,
+      errors: [{
+        message: ERROR_MSG
+      }]
+    }
+  ]
+})
