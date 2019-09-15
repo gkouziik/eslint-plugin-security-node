0a6e8a3b0f4a9936b8ae425fadf55886c9cd0e73
John Gkouz <gkouziik@gmail.com>
2019-09-16 00:04:16 +0300

    #23 Rule tested

--- tests/lib/rules/detect-html-injection.js
+++ tests/lib/rules/detect-html-injection.js
@@ -2,36 +2,36 @@
  * @fileoverview Detect html injection
  * @author Gkouziik
  */
-"use strict";
 
-//------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
+'use strict'
 
-var rule = require("../../../lib/rules/detect-html-injection"),
+var rule = require('../../../lib/rules/detect-html-injection')
+var RuleTester = require('eslint').RuleTester
+const ERROR_MSG = 'detect document.write() with non Literal argument'
+const valid = 'document.write("foo");'
+const invalid = 'var name = document.URL.indexof("name=") + 5; document.write("Welcome " + document.URL.substring(name, document.URL.length));'
+const invalidSecond = 'var foo = url; document.write(foo);'
+var ruleTester = new RuleTester()
+ruleTester.run('detect-html-injection', rule, {
 
-    RuleTester = require("eslint").RuleTester;
+  valid: [
+    {
+      code: valid
+    }
+  ],
 
-
-//------------------------------------------------------------------------------
-// Tests
-//------------------------------------------------------------------------------
-
-var ruleTester = new RuleTester();
-ruleTester.run("detect-html-injection", rule, {
-
-    valid: [
-
-        // give me some code that won't trigger a warning
-    ],
-
-    invalid: [
-        {
-            code: "",
-            errors: [{
-                message: "Fill me in.",
-                type: "Me too"
-            }]
-        }
-    ]
-});
+  invalid: [
+    {
+      code: invalid,
+      errors: [{
+        message: ERROR_MSG
+      }]
+    },
+    {
+      code: invalidSecond,
+      errors: [{
+        message: ERROR_MSG
+      }]
+    }
+  ]
+})
