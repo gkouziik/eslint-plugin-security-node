498cac3d95f54a0071f9faae3edcd6c39c2a7e81
John Gkouz <gkouziik@gmail.com>
2019-09-17 04:02:53 +0300

    #22 Rule Tested

--- tests/lib/rules/detect-nosql-injection.js
+++ tests/lib/rules/detect-nosql-injection.js
@@ -2,36 +2,25 @@
  * @fileoverview detect NOsql injection
  * @author Gkouziik
  */
-"use strict";
+'use strict'
 
-//------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
+var rule = require('../../../lib/rules/detect-nosql-injection')
+var RuleTester = require('eslint').RuleTester
+const ERROR_MSG = 'detect $where'
+const invalid = 'db.collection("users").else({$where: "this.canceledOrders > " + req.body.canceled}).each(function(err, doc){console.log(doc);})'
+var ruleTester = new RuleTester()
+ruleTester.run('detect-nosql-injection', rule, {
 
-var rule = require("../../../lib/rules/detect-nosql-injection"),
+  valid: [
 
-    RuleTester = require("eslint").RuleTester;
+  ],
 
-
-//------------------------------------------------------------------------------
-// Tests
-//------------------------------------------------------------------------------
-
-var ruleTester = new RuleTester();
-ruleTester.run("detect-nosql-injection", rule, {
-
-    valid: [
-
-        // give me some code that won't trigger a warning
-    ],
-
-    invalid: [
-        {
-            code: "",
-            errors: [{
-                message: "Fill me in.",
-                type: "Me too"
-            }]
-        }
-    ]
-});
+  invalid: [
+    {
+      code: invalid,
+      errors: [{
+        message: ERROR_MSG
+      }]
+    }
+  ]
+})
