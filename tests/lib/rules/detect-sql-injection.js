3ea247871ac5a787d03b5732deca53b6f8a0e829
John Gkouz <gkouziik@gmail.com>
2019-09-17 03:14:12 +0300

    #11 Rule Tested

--- tests/lib/rules/detect-sql-injection.js
+++ tests/lib/rules/detect-sql-injection.js
@@ -2,36 +2,29 @@
  * @fileoverview detect SQL injection
  * @author Gkouziik
  */
-"use strict";
+'use strict'
 
-//------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
+var rule = require('../../../lib/rules/detect-sql-injection')
+var RuleTester = require('eslint').RuleTester
 
-var rule = require("../../../lib/rules/detect-sql-injection"),
+const ERROR_MSG = 'detect possible SQL injection'
+const valid = 'var mysql = require("mysql"); var connection = mysql.createConnection({host: "localhost",user: "db_user",password: "secret",database: "node_app_db"});connection.connect(); connection.query("SELECT * FROM accounts WHERE username=? AND password=?", [username,passwordHash],function(err,rows,fields){});'
+const invalid = 'connection.query(foo,function(err, rows, fields) {console.log("Result = " + JSON.stringify(rows));});'
+var ruleTester = new RuleTester()
+ruleTester.run('detect-sql-injection', rule, {
 
-    RuleTester = require("eslint").RuleTester;
+  valid: [
+    {
+      code: valid
+    }
+  ],
 
-
-//------------------------------------------------------------------------------
-// Tests
-//------------------------------------------------------------------------------
-
-var ruleTester = new RuleTester();
-ruleTester.run("detect-sql-injection", rule, {
-
-    valid: [
-
-        // give me some code that won't trigger a warning
-    ],
-
-    invalid: [
-        {
-            code: "",
-            errors: [{
-                message: "Fill me in.",
-                type: "Me too"
-            }]
-        }
-    ]
-});
+  invalid: [
+    {
+      code: invalid,
+      errors: [{
+        message: ERROR_MSG
+      }]
+    }
+  ]
+})
